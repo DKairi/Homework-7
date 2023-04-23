5827a390b343e4dcb9a321ab3e3267bf8cd32fd1
D'Kairi Boone <d.boone@setonhill.edu>
2023-04-23 11:50:19 -0400

    Initial Commit

---
+++ Homework 7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 7
+//
+//  Created by d.boone on 4/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
